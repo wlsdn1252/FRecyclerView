81c0c6e965f6393af6514c6d781c173bcc63ccc3
admin <wlsdn1252@naver.com>
2023-05-30 21:10:38 +0900

    데이터클래스 추가

--- app/src/main/java/com/example/frecyclerview/Word.kt
+++ app/src/main/java/com/example/frecyclerview/Word.kt
@@ -1,9 +1,14 @@
 package com.example.frecyclerview
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 // 어댑터클래스에서 사용될 데이터 타입 정의?
 // 홀딩할 클래스?
+@Entity(tableName = "word")
 data class Word(
     val text:String,
     val mean:String,
-    val type:String
+    val type:String,
+    @PrimaryKey(autoGenerate = true)val id: Int = 0,
 )
